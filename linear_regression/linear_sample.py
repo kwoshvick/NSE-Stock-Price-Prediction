6a3e77af87e188edca3961d8b5f94ea6adde2912
kwoshvick <kwoshvick@gmail.com>
2018-04-03 14:45:42 +0300

    linear regression model

--- linear_regression/linear_sample.py
+++ linear_regression/linear_sample.py
@@ -1,35 +1,33 @@
+import quandl
 import pandas as pd
 import numpy as np
+import datetime
+
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing, cross_validation, svm
 
-
-df = pd.read_csv('SCOM.csv')
-
-
-df_close = df[[3]]
+df = quandl.get("WIKI/AMZN")
 
 
+
+
+df = df[['Adj. Close']]
+
+# print(df)
+#
+# exit()
 
 forecast_out = int(30) # predicting 30 days into future
-
-
-df['Prediction'] = df_close.shift(-forecast_out) #  label column with data shifted 30 units up
-
-# print(df.tail())
+df['Prediction'] = df[['Adj. Close']].shift(-forecast_out) #  label column with data shifted 30 units up
 
 X = np.array(df.drop(['Prediction'], 1))
 X = preprocessing.scale(X)
 
-
-
 X_forecast = X[-forecast_out:] # set X_forecast equal to last 30
 X = X[:-forecast_out] # remove last 30 from X
 
-
 y = np.array(df['Prediction'])
 y = y[:-forecast_out]
-
 
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
